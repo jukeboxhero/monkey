1548c7d5c9d03abd6f72fdcb27998aaf13d2a1ce
jukeboxHero <sholton311@gmail.com>
2021-05-26 20:37:03 -0500

    Add parser, environment, evaluator

--- repl/repl.go
+++ repl/repl.go
@@ -5,13 +5,15 @@ import (
   "fmt"
   "io"
   "monkey/lexer"
-  "monkey/token"
+  "monkey/parser"
+  "monkey/evaluator"
 )
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
   scanner := bufio.NewScanner(in)
+  env := object.NewEnvironment()
 
   for {
     fmt.Printf(PROMPT)
@@ -22,9 +24,24 @@ func Start(in io.Reader, out io.Writer) {
 
     line := scanner.Text()
     l := lexer.New(line)
+    p := parser.New(l)
 
-    for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-      fmt.Printf("%+v\n", tok)
+    program := p.ParseProgram()
+    if len(p.Errors()) != 0 {
+      printParserErrors(out, p.Errors())
+      continue
     }
+
+    evaluated := evaluator.Eval(program, env)
+    if evaluated != nil {
+      io.WriteString(out, evaluated.Inspect())
+      io.WriteString(out, "\n")
+    }
+  }
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+  for _, msg := range errors {
+    io.WriteString(out, "\t" + msg + "\n")
   }
 }
